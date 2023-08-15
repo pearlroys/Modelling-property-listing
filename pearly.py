3c130ca3fb6db0b5bc380438aab300d29f839b3e
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-15 13:24:17 +0100

    code

--- pearly.py
+++ pearly.py
@@ -1,21 +1,12 @@
-# Create bins for months
-bins = [0, 25, 50, 75, 100]  # You can adjust the bin ranges as needed
-
-# Assign bins to months
-df['Month Bins'] = pd.cut(df['Months'], bins=bins)
-
-# Create a pivot table to count NPS categories for each bin
-pivot_df = df.pivot_table(index='Month Bins', columns='NPS', aggfunc='size', fill_value=0)
-
-# Create a bar plot using Seaborn
+# Create a violin plot using Seaborn
 sns.set(style="whitegrid")
 plt.figure(figsize=(10, 6))
-pivot_df.plot(kind='bar', stacked=True, color=sns.color_palette("Set3", n_colors=3))
+sns.violinplot(x="Advisor Team", y="NPS", data=df, palette="Set3")
 
 # Add labels and title
-plt.xlabel('Months')
-plt.ylabel('Count')
-plt.title('NPS Categories by Months')
+plt.xlabel('Advisor Team Value')
+plt.ylabel('NPS Category')
+plt.title('NPS Categories by Advisor Team')
 
 # Show the plot
 plt.show()
