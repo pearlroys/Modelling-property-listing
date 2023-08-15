c50252617444ce32e33dfd9f35cb2c949813b4e3
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-15 13:38:22 +0100

    code

--- pearly.py
+++ pearly.py
@@ -1,15 +1,15 @@
-# Create a pivot table to count NPS categories for each Advisor Team value
-pivot_df = df.pivot_table(index='Advisor Team', columns='NPS', aggfunc='size', fill_value=0)
+# Create a pivot table to count NPS categories for each Gender
+pivot_df = df.pivot_table(index='Gender', columns='NPS', aggfunc='size', fill_value=0)
 
 # Create a bar plot using Seaborn
 sns.set(style="whitegrid")
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(8, 6))
 pivot_df.plot(kind='bar', stacked=True, color=sns.color_palette("Set3", n_colors=3))
 
 # Add labels and title
-plt.xlabel('Advisor Team Value')
+plt.xlabel('Gender')
 plt.ylabel('Count')
-plt.title('NPS Categories by Advisor Team')
+plt.title('NPS Categories by Gender')
 
 # Show the plot
 plt.show()
