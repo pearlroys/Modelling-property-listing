28a44918e44245be645997fcc79f89a864c7c45b
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-24 14:44:50 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,18 +1,37 @@
-# Create a pivot table to count NPS categories for each Product
-pivot_df = df.pivot_table(index='Product', columns='NPS', aggfunc='size', fill_value=0)
+import lightgbm as lgb
+import shap
+import pandas as pd
+import numpy as np
 
-# Transpose the pivot table for stacked line plot
-stacked_df = pivot_df.transpose()
+# Assuming X_train, y_train, and categorical_cols are defined
+model = lgb.LGBMClassifier(objective='multiclass', num_class=3, categorical_feature=categorical_cols)
+model.fit(X_train, y_train)
 
-# Create a stacked line plot using Seaborn
-sns.set(style="whitegrid")
-plt.figure(figsize=(10, 6))
-stacked_df.plot(kind='line', stacked=True, color=sns.color_palette("Set3", n_colors=3))
+# Choose a specific instance for which you want to explain the prediction
+sample_idx = 0  # Choose the index of the sample you want to analyze
+sample = X_test.iloc[sample_idx]
 
-# Add labels and title
-plt.xlabel('NPS Category')
-plt.ylabel('Count')
-plt.title('Stacked Line Plot of NPS Categories for Product ("prod_book")')
+# Convert the predicted class from string to float
+predicted_class = float(model.predict(sample.values.reshape(1, -1))[0])
 
-# Show the plot
-plt.show()
+# Calculate SHAP values for all features
+explainer = shap.TreeExplainer(model)
+shap_values_all = explainer.shap_values(X_test)
+
+# Select only the "worker" related features
+worker_feature_names = ['worker', 'worker_duration', 'worker_skill', 'worker_switch']
+worker_feature_indices = [X_test.columns.get_loc(col) for col in worker_feature_names]
+
+# Select SHAP values for "worker" related features
+shap_values_worker = shap_values_all[int(predicted_class)][:, worker_feature_indices]
+
+# Create a DataFrame from SHAP values
+shap_values_fl = pd.DataFrame(shap_values_worker, columns=worker_feature_names)
+
+# Select the instance's data and SHAP values for "worker" related features
+sample_worker_features = sample[worker_feature_names].values
+shap_values_sample = shap_values_fl.loc[sample_idx].values
+
+# Visualize SHAP values using force plot for the chosen instance and features
+shap.initjs()
+shap.force_plot(explainer.expected_value[int(predicted_class)], shap_values_sample, sample_worker_features, feature_names=worker_feature_names)
