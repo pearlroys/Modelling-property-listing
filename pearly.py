644fed49fd1f7a91633432b2d0e0a3a8ac36ce4f
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-24 15:52:29 +0100

    dd

--- pearly.py
+++ pearly.py
@@ -1,12 +1,3 @@
-sample_idx = 0  # Choose the index of the observation you want to analyze
-sample = X_test.iloc[sample_idx]
-
-# Calculate SHAP values for the chosen observation
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(sample.values.reshape(1, -1))
-
-# Create a DataFrame to display the SHAP values in a tabular format
-shap_df = pd.DataFrame(data=shap_values[0], columns=X_test.columns)
-
-# Display the DataFrame containing SHAP values for the chosen observation
-print(shap_df)
+row = 8
+shap.waterfall_plot(shap.Explanation(values=shap_values[0][row], 
+feature_names=X_test.columns.tolist()))
