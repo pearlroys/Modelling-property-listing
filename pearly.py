2cce4bb45a37c593e671274aaf39072dca459ebf
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-24 19:43:32 +0100

    hhh

--- pearly.py
+++ pearly.py
@@ -2,40 +2,73 @@ Sub CreatePresentation()
     Dim pptApp As Object
     Dim pptPresentation As Object
     Dim pptSlide As Object
-    
-    ' Create a new PowerPoint application
+
+    ' Create PowerPoint Application
     Set pptApp = CreateObject("PowerPoint.Application")
     pptApp.Visible = True
-    
-    ' Create a new presentation
+
+    ' Create a New Presentation
     Set pptPresentation = pptApp.Presentations.Add
-    
+
     ' Slide 1: Introduction
     Set pptSlide = pptPresentation.Slides.Add(1, ppLayoutTitle)
-    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Enhancing Emergency Healthcare with Machine Learning"
-    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "How can ML improve hospital admission decisions?"
-    
-    ' Slide 2: Problem Statement
-    Set pptSlide = pptPresentation.Slides.Add(2, ppLayoutText)
-    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Problem Statement"
-    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Accurate hospital admission prediction in ED. Relying on Triage can lead to human errors."
-    
-    ' Slide 3: Dataset and Data Quality
-    Set pptSlide = pptPresentation.Slides.Add(3, ppLayoutText)
-    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "About the Dataset"
-    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Synthetic data: Represents real patient records. Ensured data quality and addressed inconsistencies."
-    
-    ' Slide 4: Insights and Patterns
-    Set pptSlide = pptPresentation.Slides.Add(4, ppLayoutText)
-    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Exploring Data Patterns"
-    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Insights: Diverse distributions across outcomes. Impact: Data distribution disparities on model accuracy?"
-    
-    ' Slide 5: Impact and Conclusion
-    Set pptSlide = pptPresentation.Slides.Add(5, ppLayoutText)
-    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Notable Observations and Conclusion"
-    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Findings: ICD codes and TFC impact predictions. Ethical Balance: Balancing model accuracy and ethical concerns."
-    
-    ' Clean up
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Introduction"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Using Machine Learning to improve emergency healthcare diagnostics."
+
+    ' Slide 2: About the Dataset
+    Set pptSlide = pptPresentation.Slides.Add(2, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "About the Dataset"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Synthetic dataset, 100K rows, 18 columns. Data dictionary provides column definitions."
+
+    ' Slide 3: Data Inspection and Visualization
+    Set pptSlide = pptPresentation.Slides.Add(3, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Data Inspection and Visualization"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Bar plots reveal insights: age, department codes, admission trends."
+
+    ' Slide 4: Model Training and Features
+    Set pptSlide = pptPresentation.Slides.Add(4, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Model Training and Features"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Developed predictive models using XGBoost and LightGBM."
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs.Add
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs(2).Text = "Influential features: ICD10, TFC (Treatment Function Code)."
+
+    ' Slide 5: Model Performance and Adjustments
+    Set pptSlide = pptPresentation.Slides.Add(5, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Model Performance and Adjustments"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Achieved high accuracy with influential features."
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs.Add
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs(2).Text = "Robust performance without specific features."
+
+    ' Slide 6: Discussion of Results
+    Set pptSlide = pptPresentation.Slides.Add(6, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Discussion of Results"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Age, ICD codes, TFC significantly influence admissions."
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs.Add
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs(2).Text = "Model's adaptability in various scenarios."
+
+    ' Slide 7: Limitations of the Study
+    Set pptSlide = pptPresentation.Slides.Add(7, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Limitations of the Study"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Dataset quality, potential misclassification of outcome."
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs.Add
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs(2).Text = "Generalizability, changing healthcare practices."
+
+    ' Slide 8: Business and Health Impact
+    Set pptSlide = pptPresentation.Slides.Add(8, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Business and Health Impact"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Cost savings, resource management, supporting triage nurses."
+
+    ' Slide 9: Conclusion
+    Set pptSlide = pptPresentation.Slides.Add(9, ppLayoutTitle)
+    pptSlide.Shapes(1).TextFrame.TextRange.Text = "Conclusion"
+    pptSlide.Shapes(2).TextFrame.TextRange.Text = "Predictive model for hospital admissions using accessible data."
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs.Add
+    pptSlide.Shapes(2).TextFrame.TextRange.Paragraphs(2).Text = "Empowering healthcare decisions with data-driven insights."
+
+    ' Continue adding slides for each section...
+    ' Slide 10 to Slide 12
+
+    ' Close and Clean Up
     Set pptSlide = Nothing
     Set pptPresentation = Nothing
     Set pptApp = Nothing
