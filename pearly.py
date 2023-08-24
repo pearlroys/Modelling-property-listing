8a098656f5c4b5939b582e6d94d78e97e8353a2a
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-24 19:24:13 +0100

    lddl

--- pearly.py
+++ pearly.py
@@ -1,3 +1,42 @@
-row = 8
-shap.waterfall_plot(shap.Explanation(values=shap_values[0][row], 
-feature_names=X_test.columns.tolist()))
+Sub CreatePresentation()
+    Dim pptApp As Object
+    Dim pptPresentation As Object
+    Dim pptSlide As Object
+    
+    ' Create a new PowerPoint application
+    Set pptApp = CreateObject("PowerPoint.Application")
+    pptApp.Visible = True
+    
+    ' Create a new presentation
+    Set pptPresentation = pptApp.Presentations.Add
+    
+    ' Slide 1: Introduction
+    Set pptSlide = pptPresentation.Slides.Add(1, ppLayoutTitle)
+    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Enhancing Emergency Healthcare with Machine Learning"
+    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "How can ML improve hospital admission decisions?"
+    
+    ' Slide 2: Problem Statement
+    Set pptSlide = pptPresentation.Slides.Add(2, ppLayoutText)
+    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Problem Statement"
+    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Accurate hospital admission prediction in ED. Relying on Triage can lead to human errors."
+    
+    ' Slide 3: Dataset and Data Quality
+    Set pptSlide = pptPresentation.Slides.Add(3, ppLayoutText)
+    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "About the Dataset"
+    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Synthetic data: Represents real patient records. Ensured data quality and addressed inconsistencies."
+    
+    ' Slide 4: Insights and Patterns
+    Set pptSlide = pptPresentation.Slides.Add(4, ppLayoutText)
+    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Exploring Data Patterns"
+    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Insights: Diverse distributions across outcomes. Impact: Data distribution disparities on model accuracy?"
+    
+    ' Slide 5: Impact and Conclusion
+    Set pptSlide = pptPresentation.Slides.Add(5, ppLayoutText)
+    pptSlide.Shapes.Title.TextFrame.TextRange.Text = "Notable Observations and Conclusion"
+    pptSlide.Shapes.Placeholders(2).TextFrame.TextRange.Text = "Findings: ICD codes and TFC impact predictions. Ethical Balance: Balancing model accuracy and ethical concerns."
+    
+    ' Clean up
+    Set pptSlide = Nothing
+    Set pptPresentation = Nothing
+    Set pptApp = Nothing
+End Sub
