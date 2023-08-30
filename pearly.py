c6f4e78bc1df839741b142c08b4fe8fd0376065f
Pearl Roys <pearl_chisom@yahoo.com>
2023-08-30 13:16:29 +0100

    code

--- pearly.py
+++ pearly.py
@@ -1,72 +1,22 @@
-Of course, I can help you create an award-winning presentation using the provided information. Since you're looking for a concise presentation, I'll summarize the key points from each section and present them as bullet points on each slide. Here's how your presentation could be structured:
+# Group by advisor skill and calculate counts of predicted and actual promoters
+advisor_skills_grouped = final_df.groupby('advisor_skill').apply(lambda group: {
+    'predicted_promoters': (group['predicted'] == 'promoter').sum(),
+    'actual_promoters': (group['actual'] == 'promoter').sum()
+}).reset_index()
 
-**Slide 1: Introduction**
-- Challenge: Identifying patients needing hospital admission quickly.
-- Goal: Use ML to enhance healthcare diagnostics and ED triage.
-- Importance: Reducing human-related errors and optimizing resources.
 
-**Slide 2: About the Dataset**
-- Data Privacy: Confidential patient data, synthetic, not specific.
-- Dataset Details: 100K rows, 18 columns, comprehensive data dictionary.
-- Initial Steps: Data preprocessing, identifying inconsistencies.
+advisor_skills_grouped['ratio_predicted_to_actual'] = (
+    advisor_skills_grouped['predicted_promoters'] / advisor_skills_grouped['actual_promoters']
+)
 
-**Slide 3: Data Visualization Insights**
-- TFC Code '300': Signifies internal medicine, common among admissions.
-- ICD10 Codes: Linked to non-admitted cases, puzzling variations.
-- Average Stay: Admitted - 250 mins, Not Admitted - 150 mins.
-- Age Bands: '65-84' most admissions, '25-44' and '1-17' fewest.
-- HRG Categories: 'Low' category most admissions.
-- Day & Time: Saturday peak admissions, '9-12' time range peak.
+import matplotlib.pyplot as plt
 
-**Slide 4: Data Leakage & Features**
-- Data Leakage Concerns: Time-related columns analyzed.
-- AE_Time_Mins: Time from arrival to treatment, no overlap.
-- AE_Arrival_Mode: Retained for insights, careful judgment.
-
-**Slide 5: Model Achievements**
-- Remarkable Results: Accuracy, precision, F1 scores.
-- Influence of Features: ICD10, TFC enhanced predictions.
-- XGBoost Performance: 83.23% accuracy without specific features.
-
-**Slide 6: Model Adaptability**
-- Controversial Columns Removed: Model accuracy at 77.87%.
-- Model's Strength: Adaptability under diverse circumstances.
-
-**Slide 7: Discussion - Age & Predictions**
-- Age's Influence: Consistent with prior research.
-- Missing Medical Tests: Impact on prediction accuracy.
-- ICD Codes & Comorbidities: High accuracy, deviation from other studies.
-
-**Slide 8: Enhancing Accuracy**
-- Exploratory Approach: Alternative methods for accuracy.
-- Techniques Used: Lasso regularization, random forests, gradient boosting, neural networks.
-- Overfitting Addressed: Each technique's role in enhancing prediction.
-
-**Slide 9: Optimal Timing for Model**
-- Timing Considerations: Model's effectiveness upon arrival or later.
-- Tests & Investigations: Applying the model during patient's stay.
-
-**Slide 10: Limitations & Challenges**
-- Data Quality: Linked to model accuracy.
-- Synthetic Nature: Real-world applicability challenge.
-- Misclassification & Bias: Impact on model predictions.
-- Correlation vs. Causation: Complex scenarios' complexity.
-
-**Slide 11: Generalizability & Updates**
-- Model Generalizability: Varying contexts, cautious interpretation.
-- Evolving Healthcare Practices: Continuous updates for relevance.
-
-**Slide 12: Business & Health Impact**
-- Cost Impact & Savings: Potential for cost reduction.
-- Resource Management: Streamlined admission process, reduced bed blocking.
-- Supporting Triage Nurses: Complementary role of the model.
-- Similar Systems' Impact: Positive results from eTriage.
-
-**Slide 13: Conclusion**
-- Model's Potential: Transformative impact on healthcare.
-- Practicality: Utilizing everyday data for informed decisions.
-- Highlighting Strengths: Reliable predictions from simple data.
-
-This outline covers the key highlights from your provided information and presents them as concise bullet points for each slide. You can expand on these points further in your actual presentation if needed. Remember to use visuals and graphics to enhance the presentation's appeal. Good luck with your award-winning presentation!
-\Problem Statement:
-In the fast-paced environment of busy Emergency Departments (EDs), accurately and promptly identifying patients in need of hospital admission remains a challenge. We want to use Machine Learning (ML) to enhance this process, improving patient care and resource management.
+# Plot the ratios for each advisor skill
+plt.figure(figsize=(10, 6))
+plt.bar(advisor_skills_grouped['advisor_skill'], advisor_skills_grouped['ratio_predicted_to_actual'])
+plt.xlabel('Advisor Skill')
+plt.ylabel('Ratio of Predicted Promoters to Actual Promoters')
+plt.title('Impact of Advisor Skills on Predicted/Actual Promoters Ratio')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
