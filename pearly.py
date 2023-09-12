b1b6758750696c257ae971dede2997b546578e99
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-12 07:54:26 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,47 +1,10 @@
 
 
-**Promoters:**
+"Hi Milan,
 
-- **Advisor Team Volume**: Increasing advisor team volume positively impacts Shap values for promoters.
+I hope you're well. I wanted to share some good news—I've been offered a new job, and it's set to start on the 25th. Given this, I was wondering if it would be alright for me to speak with Joe, Saul, and Isaac about the possibility of ending my internship on the 22nd. My last presentation is scheduled for next week, and I believe this timeline would allow for a smoother transition.
 
-- **Advisor Team Switch (== 1)**: A positive impact on Shap values for promoters is observed when there is a switch in the advisor team.
+Your guidance on this matter would be greatly appreciated.
 
-- **Advisor Tenure**: The impact on Shap values for promoters varies with tenure months. Higher tenure months have a stronger influence.
+Thank you.
 
-- **Large Workload (LWT)**: High LWT values lead to negative Shap values, implying that higher workload counts tend to negatively affect promoters.
-
-- **Age**: Both low and high age values have varying impacts on promoters. Lower age values tend to have a positive effect on Shap values.
-
-- **Summary Plot**: The summary plot reveals both positive and negative impacts of various factors on Shap values for promoters, indicating complex dependencies.
-
-**Passives:**
-
-- **Advisor Team Volume**: Decreasing advisor team volume positively impacts Shap values for passives.
-
-- **Advisor Team Switch (== 0)**: A negative impact on Shap values for passives is observed when there is no switch in the advisor team.
-
-- **Advisor Tenure**: The impact on Shap values for passives varies with tenure months. Lower tenure months have a stronger influence.
-
-- **Age**: Age plays a significant role in classifying individuals as passives.
-
-- **Advisor Skill**: Advisor skill is a crucial factor in determining passives.
-
-- **Dependence Plot**: The dependence plot highlights that advisor tenure between 0-25 months has the highest positive impact on Shap values for passives.
-
-- **Summary Plot**: Both low age and high advisor values are associated with reduced Shap values for passives.
-
-**Detractors:**
-
-- **Tenure Months**: Decreasing tenure months have a positive impact on Shap values for detractors, indicating that lower advisor tenure months result in more detractors.
-
-- **Advisor Team Volume (AD_team.vol == 0)**: A negative impact on Shap values for detractors is observed when the advisor team volume is zero.
-
-- **Advisor Skill**: Advisor skill significantly influences individuals to become detractors.
-
-- **Age**: Age has a more substantial impact on detractors compared to promoters.
-
-- **Team Switch**: Switching teams has a positive marginal impact on becoming a detractor.
-
-- **Summary Plot**: The summary plot shows both positive and negative impacts of various factors on Shap values for detractors, suggesting the need for individual or smaller group analysis.
-
-This refined summary provides a clearer overview of the factors influencing each category (Promoters, Passives, Detractors) and their respective Shap values.
