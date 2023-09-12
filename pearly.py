71a35f31af8461b1aa76d3bd4e6b76961073df7b
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-12 11:42:35 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,10 +1,21 @@
+Subject: Resignation Letter - Health Data Scientist Internship
 
+Dear Isaac and Milan,
 
-"Hi Milan,
+I hope this message finds you well. I am writing to officially resign from my position as a Health Data Scientist Intern at Bupa, effective September 22nd, 2023. It is with mixed emotions that I make this decision, but I wanted to express my heartfelt gratitude for the incredible experience I've had during my three-month internship at Bupa.
 
-I hope you're well. I wanted to share some good news—I've been offered a new job, and it's set to start on the 25th. Given this, I was wondering if it would be alright for me to speak with Joe, Saul, and Isaac about the possibility of ending my internship on the 22nd. My last presentation is scheduled for next week, and I believe this timeline would allow for a smoother transition.
+I want to begin by extending my profuse thanks and appreciation for the warm welcome, guidance, and support I received from both of you. Your mentorship and the opportunities you provided have been instrumental in my growth and development as a data scientist. I am truly grateful for the chance to learn from such accomplished professionals.
 
-Your guidance on this matter would be greatly appreciated.
+I would also like to commend everyone on Saul's team, including Saul himself, Milan, Kayla, Rohan, and Adam, for their unwavering support and camaraderie. The collaborative environment within the Manchester team has been inspiring, and I feel fortunate to have had the opportunity to work alongside such talented individuals.
 
-Thank you.
+My decision to leave Bupa is driven by a new job offer with Octopus Energy, a role that aligns with my career goals and aspirations. While I am excited about this new chapter in my professional journey, I will carry the lessons and experiences from my time at Bupa with me. I want to emphasize that this decision was not made lightly, and I am sincerely thankful for the understanding and support I have received from everyone at Bupa.
 
+As I move forward, I want to assure you that I will keep an eye out for opportunities to collaborate and reconnect with the Bupa team in the future. The relationships I've built and the knowledge I've gained here are invaluable, and I hope to stay connected with this wonderful community.
+
+Once again, thank you for everything. I genuinely appreciate the trust you placed in me and the kindness you showed throughout my internship. Bupa will always hold a special place in my heart, and I wish the team continued success in all your endeavors.
+
+Please do not hesitate to reach out if there are any tasks or handovers required before my departure on the 22nd of September. I am committed to ensuring a smooth transition.
+
+Warm regards,
+
+[Your Name]
