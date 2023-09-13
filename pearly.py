2108013224d6be86138d53172336a4dd6a6aba6e
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-13 04:05:43 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,117 +1,161 @@
-Creating an impactful PowerPoint presentation for your internship review is essential to effectively communicate your accomplishments and contributions. Here's an enhanced outline to help you structure your presentation:
+Subtitle: Visualizing NPS Trends
 
-**Slide 1: Title Slide**
-- Title: Internship Review Presentation
-- Your Name
-- Date
-- Company Logo (if applicable)
+Project Overview:
 
-**Slide 2: Introduction**
-- Title: Introduction
-- Briefly introduce yourself, including your academic background and major.
-- Mention the organization where you completed your internship.
-- Set the stage for what your presentation will cover.
+Explored NPS (Net Promoter Score) trends using Power BI.
+Conducted fortnightly analysis of Promoters, Passives, Detractors, and NPS scores.
+Enabled tracking of trends over time.
+Key Insights:
 
-**Slide 3: Internship Overview**
-- Title: Internship Overview
-- Describe the department or team you worked with during your internship.
-- Highlight the internship's duration and the broader context of your role.
+Examined how various factors impact NPS scores.
+Explored the influence of factors like response times, conditions, and tenure duration.
+Provided valuable insights into customer sentiment.
+Slide Title: Skills Learned in Power BI
 
-**Slide 4: Objectives and Expectations**
-- Title: Objectives and Expectations
-- Outline the objectives you set at the beginning of your internship.
-- Explain what you expected to achieve and learn during your internship.
+Data Manipulation:
 
-**Slide 5: Main NPS Model Project**
-- Title: Main NPS Model Project
-- Dive into the core project you worked on during your internship.
-- Explain its significance to the organization and its goals.
-- Mention the project's duration and any team members involved.
+Proficient in using DAX (Data Analysis Expressions) code for complex calculations.
+Creating new tables and measures to derive valuable insights from data.
+Data Integration:
 
-**Slide 6: Your Role and Contributions**
-- Title: Your Role and Contributions
-- Describe your specific role and responsibilities within the project.
-- Highlight your major contributions and tasks you completed.
-- Emphasize how your work aligned with the project's success.
+Skillful in establishing relationships between different data tables in Power BI.
+Importing, exporting, and refreshing data to ensure it remains up-to-date.
 
-**Slide 7: Methodologies and Tools**
-- Title: Methodologies and Tools
-- Discuss the methodologies, techniques, and tools you utilized for the project.
-- Explain any data collection, cleaning, or preprocessing steps.
-- Mention the machine learning algorithms or programming languages you employed.
+Certainly, here's the content divided into two slide pages:
 
-**Slide 8: Data Analysis and Insights**
-- Title: Data Analysis and Insights
-- Present the key findings and insights derived from your project.
-- Use visualizations, charts, or data snapshots to illustrate your points.
-- Discuss any recommendations or actions resulting from your analysis.
+**Slide Page 1: Technical Challenge Overview**
 
-**Slide 9: Impact on the Organization**
-- Title: Impact on the Organization
-- Explain how your project positively influenced the organization.
-- Share any metrics, KPIs, or improvements attributed to your work.
-- Include feedback or testimonials from colleagues or superiors.
+- **Project Leadership:**
+  - Led a team of 8 members on a project involving hospital historical data. and won.
+  - Objective: Create a predictive model for hospital admissions using common ED data.
 
-**Slide 10: Power BI Project**
-- Title: Power BI Project
-- Introduce the Power BI project you executed.
-- Describe its objectives and how it complements the NPS model project.
-- Provide a high-level overview of the data sources and visualizations created.
+- **Key Findings:**
+  - Age emerged as a significant factor affecting admission rates, aligning with prior research.
+  - Absence of specific medical test details (biochemical markers, coagulation tests) raised accuracy concerns.
 
-**Slide 11: Data Visualization in Power BI**
-- Title: Data Visualization in Power BI
-- Showcase selected Power BI visualizations.
-- Explain their role in visualizing NPS trends and facilitating decision-making.
-- Highlight any interactive features or user-friendly aspects.
+- **Surprising Results:**
+  - Unusually high accuracy achieved by utilizing ICD codes and patient comorbidities.
+  - Deviated from results in previous studies, highlighting innovation.
 
-**Slide 12: Technical Challenges and Solutions**
-- Title: Technical Challenges and Solutions
-- Discuss any technical challenges you encountered.
-- Share your problem-solving strategies and how you overcame obstacles.
-- Reflect on the skills and knowledge gained from these experiences.
+**Slide Page 2: Innovation and Impact**
 
-**Slide 13: Personal and Professional Growth**
-- Title: Personal and Professional Growth
-- Reflect on how your internship contributed to your personal and professional development.
-- Mention the skills, competencies, or insights you've acquired.
-- Discuss how this growth aligns with your future career goals.
+- **Innovation and Techniques:**
+  - Explored various methods: Lasso regularization, random forests, gradient boosting, and neural networks.
+  - Aimed to enhance accuracy and combat overfitting.
 
-**Slide 14: Current Life Expectancy Project**
+- **Timing Consideration:**
+  - Deliberated on the timing of model application: upon patient arrival or later in their ED stay.
+  - Considered impact on prediction effectiveness.
+
+- **Potential Impact:**
+  - Tool excels in delivering reliable predictions using simple ED visit data.
+  - Acknowledges limitations due to missing details.
+
+- **Benefits and Transformation:**
+  - Potential to reduce overtriage, leading to substantial cost savings.
+  - Optimizes resource management and supports triage nurses.
+  - Similar systems like eTriage have shown promise in enhancing patient flow and staff efficiency.
+  - Predictive model has the potential to transform healthcare operations and patient care by reducing overtriage, optimizing resources, and supporting healthcare professionals.
+
+Feel free to use these two slide pages as part of your presentation. You can adjust the formatting and design as needed to create visually appealing slides.
+
+
+
+Certainly, here's the content formatted for two PowerPoint slides:
+
+**Slide 1: Skills Gained During the Internship**
+
+- Title: Skills Gained During the Internship
+- Bullet Points:
+  - Technical Skills:
+    - Proficiency in SAS for data analysis and statistical modeling.
+    - Mastery of Power BI for data visualization and reporting.
+    - Expertise in data manipulation, including table joins and feature engineering.
+  - Collaboration and Teamwork:
+    - Worked collaboratively with cross-functional teams to understand their data needs and requirements.
+    - Effective communication and collaboration ensured the successful completion of tasks and project goals.
+  - Soft Skills:
+    - Communication: Developed strong communication skills, both written and verbal, to convey complex technical concepts effectively.
+    - Problem-Solving: Honed problem-solving skills by addressing technical challenges and finding innovative solutions.
+    - Adaptability: Demonstrated adaptability in a dynamic work environment, embracing new tools and methodologies.
+    - Time Management: Efficiently managed time and resources to meet project deadlines.
+    - Attention to Detail: Developed a keen eye for detail to ensure data accuracy and quality.
+
+**Slide 2: Alignment with Future Goals and Continued Growth**
+
+- Title: Alignment with Future Goals and Continued Growth
+- Bullet Points:
+  - Alignment with Future Goals:
+    - Aspiring Data Scientist: Proficiency in SAS and data analysis is crucial for data science roles.
+    - Business Intelligence Analyst: Mastery of Power BI enhances my ability to provide valuable insights through data visualization.
+    - Team Leader: Leadership and collaboration skills gained while leading a team of 8 members are essential for future leadership roles.
+  - Continued Growth:
+    - I am committed to continuous learning and growth in the field of data analytics.
+    - I plan to expand my skill set further and pursue advanced certifications in SAS and data science.
+    - My internship experience has laid a solid foundation for my career, and I look forward to contributing my skills and knowledge to future endeavors.
+
+Feel free to use these slides as a template for your internship review presentation. You can add visuals, graphics, and your personal touch to make the presentation engaging and informative
+
+Certainly, here's the content formatted for two PowerPoint slides:
+
+**Slide 1: Current Life Expectancy Project**
+
 - Title: Current Life Expectancy Project
-- Introduce your ongoing project related to life expectancy analysis.
-- Explain its importance and specific objectives.
-- Provide context on the data sources and methodologies applied.
+- Bullet Points:
+  - Project Overview:
+    - Conducted under the guidance of the esteemed Kayla.
+    - Explored factors influencing life expectancy in various countries over a 10-year period.
+    - Analyzed lifestyle factors such as BMI, alcohol consumption, and national factors including GDP, healthcare expenditure, and disease prevalence (e.g., polio, diphtheria, measles, HIV).
+  - Methodology:
+    - Investigated connections between life expectancy predictions and Bupa population life expectancy data.
+    - Examined intriguing trends and patterns in life expectancy.
+  - Key Findings:
+    - Discovered significant correlations between lifestyle choices, disease prevalence, and life expectancy.
+    - Identified key factors contributing to variations in life expectancy across countries.
+  - Conclusion:
+    - Concluded with insights into the impact of lifestyle and healthcare factors on life expectancy.
 
-**Slide 15: Analyzing Influential Factors**
-- Title: Analyzing Influential Factors
-- Describe the factors under analysis, such as alcohol consumption, smoking habits, and BMI.
-- Share preliminary findings or trends related to these factors.
-- Mention any subpopulations or demographics you're exploring.
+**Slide 2: Impact and Future Insights**
 
-**Slide 16: Future Directions**
-- Title: Future Directions
-- Discuss the next steps for your ongoing life expectancy project.
-- Mention any anticipated outcomes, publications, or contributions to the field.
-- Share your enthusiasm for continued research and analysis.
+- Title: Impact and Future Insights
+- Bullet Points:
+  - Impact:
+    - The project sheds light on the complex interplay between lifestyle choices, healthcare, and life expectancy.
+    - Provides valuable insights for policymakers and healthcare professionals to improve public health.
+  - Future Insights:
+    - Ongoing research will delve deeper into understanding these factors.
+    - Aim to develop predictive models for life expectancy and identify interventions to enhance it.
+  - Personal Growth:
+    - The project has expanded my skills in data analysis, research, and problem-solving.
+    - It aligns with my passion for public health and data-driven decision-making.
+  - Gratitude:
+    - Express heartfelt gratitude to Kayla for mentorship and guidance throughout the project.
+    - Thank the team for their collaboration and support.
 
-**Slide 17: Conclusion and Gratitude**
-- Title: Conclusion and Gratitude
-- Summarize the key takeaways from your internship journey.
-- Express heartfelt gratitude to your mentors, colleagues, and the organization.
-- Thank your audience for their attention and support.
+Feel free to use these slides as a template for your presentation, and customize them with visuals and graphics to make them engaging and informative.
 
-**Slide 18: Questions and Answers**
-- Title: Questions and Answers
-- Open the floor for questions from the audience.
-- Encourage an interactive discussion.
+Certainly, here's the content as if you were presenting it in your own words on a single PowerPoint slide:
 
-**Slide 19: Contact Information (Optional)**
-- Title: Contact Information
-- Include your email address or LinkedIn profile for further inquiries or networking.
+[Slide: Conclusion and Gratitude]
 
-**Slide 20: Closing Remarks and Thank You**
-- Title: Closing Remarks and Thank You
-- Offer closing remarks, emphasizing your appreciation.
-- Conclude with a heartfelt thank-you to your audience.
+Title: Conclusion and Gratitude
 
-Remember to practice your presentation thoroughly, maintain a confident and engaging delivery, and be prepared to address questions from your audience. Good luck with your internship review presentation!
+- It's time to wrap up this incredible journey. I want to share some highlights and express my heartfelt gratitude.
+
+- First and foremost, to Bupa as a whole: Thank you for this fantastic opportunity.
+
+- I owe a massive thanks to the Data Analytics Team, brilliantly led by Priya, for their unwavering support throughout.
+
+- The Customer Analytics Team, under Joe's guidance, deserves a special mention for their invaluable insights.
+
+- A massive shoutout to the Data Science Team, led by the amazing Saul.
+
+- I'd like to acknowledge some extraordinary individuals who played a significant role:
+  - Adam, Rohan, Marian, Susa, Milan, Saul, Sofia, Eljon, Sarah, Ellen, the two Paolas, Suja, Trish, Isaac, Nahin, Dagmar, Tom, and the phenomenal Kayla.
+  - This list doesn't capture everyone, but you've all made an indelible mark on my journey.
+
+- Thank you for not just being colleagues, but for becoming an integral part of this incredible learning experience.
+
+- Your impact on my self-confidence and professional growth is immeasurable. It's been an honor working alongside each one of you.
+
+Feel free to copy and paste this text into your PowerPoint slide and adjust the formatting as needed.
