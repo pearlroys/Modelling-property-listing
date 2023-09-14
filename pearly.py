2cd8b66949b24f94a517e7939add034a8b2c2014
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-14 12:06:17 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,70 +1,29 @@
-Certainly, I'll provide more details and expand the content over three PowerPoint slides.
+import matplotlib.pyplot as plt
+import numpy as np
 
-**Slide 1: Main NPS Project**
+# Assuming you have a DataFrame 'df' with columns 'average_predicted_nps' and 'actual_nps'
+average_predicted_nps = df['average_predicted_nps']
+actual_nps = df['actual_nps']
 
-*Title: Main NPS Project*
+# Create an array of indices for each data point
+indices = np.arange(len(df))
 
-- **Data Sources:** In this significant project, I had the task of collecting data from a diverse range of sources, including Verint data, pre-authorization data, claims data, and more. These datasets provided critical insights into customer experiences.
+# Set the width of each bar
+bar_width = 0.35
 
-- **Learning SAS:** To effectively handle this complex data, I underwent intensive training in SAS, led by the exceptional Milan. In a relatively short time, I became proficient in SAS and Proc SQL, which significantly expedited data processing and analysis.
+# Create a bar chart for actual NPS values
+plt.bar(indices, actual_nps, bar_width, label='Actual NPS', color='green', alpha=0.7)
 
-- **Data Understanding:** Understanding the data was a pivotal step. I meticulously dissected individual columns within the datasets, deciphering their meanings and relevance to the NPS project's goals.
+# Add a horizontal line for the average predicted NPS
+plt.axhline(y=average_predicted_nps.iloc[0], color='blue', linestyle='--', label='Average Predicted NPS')
 
-**Slide 2: Skills Acquired**
+# Add labels, title, and legend
+plt.xlabel('Data Point')
+plt.ylabel('NPS')
+plt.title('Comparison of Average Predicted NPS and Actual NPS')
+plt.xticks(indices, indices)
+plt.legend()
 
-*Title: Skills Acquired*
-
-- **Technical Proficiency:** Through this project, I acquired an array of technical skills. This included mastering SAS and Proc SQL, which empowered me to manipulate and analyze data efficiently.
-
-- **Data Analysis Expertise:** I developed a keen aptitude for data cleaning, preprocessing, and complex dataset analysis. This skill set was instrumental in deriving meaningful insights.
-
-- **Machine Learning Mastery:** Leveraging machine learning techniques, I excelled in creating predictive models and fine-tuning them for optimal performance.
-
-**Slide 3: Methodologies and Impact**
-
-*Title: Methodologies and Impact*
-
-- **Data Cleaning:** Ensuring data quality and consistency through thorough cleaning was a foundational step in this project. Clean data was crucial for accurate analysis and modeling.
-
-- **Insightful Analysis:** I performed in-depth data analysis to extract actionable insights. This analytical approach revealed patterns and trends that informed our strategies.
-
-- **Optimizing eNPS:** By concatenating admission details and closely analyzing the predictions, we were able to pinpoint areas for improvement in the employee Net Promoter Score (eNPS). This led to tangible enhancements in our strategies and ultimately improved eNPS performance.
-
-Feel free to use these detailed slides as part of your presentation. You can further customize them by adding visuals, specific examples, or metrics to showcase the project's impact.
-Certainly, let's break down your experience with SHAP values and their impact into three slides:
-
-**Slide 1: Understanding SHAP Values**
-
-*Title: Understanding SHAP Values*
-
-- **Introduction to SHAP:** I delved into the world of SHAP (SHapley Additive exPlanations) values, a powerful tool for explaining machine learning models' predictions.
-
-- **Interpreting Model Outputs:** SHAP values provided me with insights into how our models made predictions. They helped uncover the contributions of each feature to individual predictions.
-
-- **Enhancing Explainability:** By utilizing SHAP values, I enhanced the explainability of our models. This not only boosted our confidence in model predictions but also enabled us to communicate model behavior more effectively.
-
-**Slide 2: Leveraging SHAP Values for Improvement**
-
-*Title: Leveraging SHAP Values for Improvement*
-
-- **Identifying Discrepancies:** I used SHAP values to scrutinize cases where the model's predictions for advisor performance in NPS differed significantly from actual results.
-
-- **Diagnosing Discrepancies:** This analysis helped diagnose areas where advisors were predicted to perform well or poorly but didn't align with real-world outcomes.
-
-- **Optimizing eNPS:** Armed with this information, we strategized on how to optimize and improve our employee Net Promoter Score (eNPS) across the organization. These insights played a pivotal role in refining our approaches.
-
-**Slide 3: Impact on eNPS and Organization**
-
-*Title: Impact on eNPS and Organization*
-
-- **Boosting eNPS:** The strategies derived from SHAP value analysis had a direct impact on eNPS scores. We witnessed a notable increase in employee satisfaction and engagement, leading to higher eNPS ratings.
-
-- **Positive Organizational Effects:** The improved eNPS scores had far-reaching effects on the organization. They correlated with higher employee retention rates, improved workplace culture, and heightened productivity.
-
-- **Continual Optimization:** This ongoing process of leveraging SHAP values and data-driven insights allowed us to continually optimize eNPS and enhance the overall employee experience.
-
-These three slides capture your journey of understanding and utilizing SHAP values, along with the positive impact they had on eNPS and the organization as a whole.
-
-to pinpoint areas for improvement, leading to better eNPS performance.
-
-Enhancing Strategies: By identifying where advisors were predicted to excel or struggle but differed from actual results, we optimized our strategies.
+# Show the chart
+plt.tight_layout()
+plt.show()
