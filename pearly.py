831cab5d643fcd1e25e0f3b020bc94f332757239
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-15 09:40:46 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,11 +1,13 @@
-# Initialize lists to store x and average values
-x_values = []
+all_x_values = []
 average_values = []
 
-# Calculate the average values for each bucket and store corresponding x-values
+# Calculate the average values for each bucket and store all x-values
 for edge in bucket_centers:
     x_in_bucket = col_x[(col_x >= edge - bucket_size / 2) & (col_x < edge + bucket_size / 2)]
     if len(x_in_bucket) > 0:
         average_y = col_y[(col_x >= edge - bucket_size / 2) & (col_x < edge + bucket_size / 2)].mean()
-        x_values.append(np.mean(x_in_bucket))
-        average_values.append(average_y)
+        all_x_values.extend(x_in_bucket)
+        average_values.extend([average_y] * len(x_in_bucket))
+
+# Create a scatter plot
+plt.scatter(all_x_values, average_values, label='Average Values')
