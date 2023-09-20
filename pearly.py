280fa9c6aed2667964224eb8c6828ed5c0b7cffc
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-20 11:20:46 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,9 +1,4 @@
-class_labels = ["Class 0", "Class 1", "Class 2"]
+filtered_df = df[['Column1', 'Column2', 'Column3']][(df['ColumnA'] == 'value1') & (df['ColumnB'] == 'value2')]
 
-# Create a confusion matrix heatmap
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=class_labels, yticklabels=class_labels)
-plt.xlabel("Predicted Labels")
-plt.ylabel("True Labels")
-plt.title("Confusion Matrix")
-plt.show()
+# Display the filtered DataFrame
+print(filtered_df)
