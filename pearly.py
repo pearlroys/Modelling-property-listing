80cd039a6765bbea6d2fc21dcf2efa1dc52225a0
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-20 11:11:56 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,44 +1,9 @@
-coeff correl type =
-VAR CorrelationValue = [coeff corr]
-VAR CorrelationType =
-    SWITCH (
-        TRUE (),
-        CorrelationValue = -1, "Perfect negative correlation",
-        CorrelationValue > -1 && CorrelationValue <= -0.8, "Very strong negative correlation",
-        CorrelationValue > -0.8 && CorrelationValue <= -0.6, "Strong negative correlation",
-        CorrelationValue > -0.6 && CorrelationValue <= -0.4, "Moderate negative correlation",
-        CorrelationValue > -0.4 && CorrelationValue <= -0.2, "Weak negative correlation",
-        CorrelationValue > -0.2 && CorrelationValue < 0, "Very weak negative correlation",
-        CorrelationValue = 0, "No correlation",
-        CorrelationValue > 0 && CorrelationValue < 0.2, "Very weak positive correlation",
-        CorrelationValue >= 0.2 && CorrelationValue < 0.4, "Weak positive correlation",
-        CorrelationValue >= 0.4 && CorrelationValue < 0.6, "Moderate positive correlation",
-        CorrelationValue >= 0.6 && CorrelationValue < 0.8, "Strong positive correlation",
-        CorrelationValue >= 0.8 && CorrelationValue < 1, "Very strong positive correlation",
-        CorrelationValue = 1, "Perfect positive correlation",
-        "Unknown"
-    )
-RETURN
-    CorrelationValue & " (" & CorrelationType & ")"
-coeff correl type =
-VAR CorrelationValue = ROUND([coeff corr], 2)
-VAR CorrelationType =
-    SWITCH (
-        TRUE (),
-        CorrelationValue = -1, "Perfect negative correlation",
-        CorrelationValue > -1 && CorrelationValue <= -0.8, "Very strong negative correlation",
-        CorrelationValue > -0.8 && CorrelationValue <= -0.6, "Strong negative correlation",
-        CorrelationValue > -0.6 && CorrelationValue <= -0.4, "Moderate negative correlation",
-        CorrelationValue > -0.4 && CorrelationValue <= -0.2, "Weak negative correlation",
-        CorrelationValue > -0.2 && CorrelationValue < 0, "Very weak negative correlation",
-        CorrelationValue = 0, "No correlation",
-        CorrelationValue > 0 && CorrelationValue < 0.2, "Very weak positive correlation",
-        CorrelationValue >= 0.2 && CorrelationValue < 0.4, "Weak positive correlation",
-        CorrelationValue >= 0.4 && CorrelationValue < 0.6, "Moderate positive correlation",
-        CorrelationValue >= 0.6 && CorrelationValue < 0.8, "Strong positive correlation",
-        CorrelationValue >= 0.8 && CorrelationValue < 1, "Very strong positive correlation",
-        CorrelationValue = 1, "Perfect positive correlation",
-        "Unknown"
-    )
-RETURN
-    CorrelationValue & " (" & CorrelationType & ")"
+class_labels = ["Class 0", "Class 1", "Class 2"]
+
+# Create a confusion matrix heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=class_labels, yticklabels=class_labels)
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.title("Confusion Matrix")
+plt.show()
