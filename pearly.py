7ab749144a38251393ad8891d44c910848d298f7
Pearl Roys <pearl_chisom@yahoo.com>
2023-09-21 11:04:25 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,1 +1,42 @@
-rows_with_patient_384 = filtered_df[filtered_df['patient id'] == 384]
+Certainly, here are two succinct points for your PowerPoint presentation:
+
+1. **Tenure Matters**: Our analysis reveals that advisor tenure plays a pivotal role in customer outcomes. Advisors with tenure ranging from 20 to 50 months consistently excel at turning customers into promoters of our services.
+
+2. **Tenure and Sentiment**: A closer look at the summary plot further validates this trend. Advisors with longer tenure are highly effective at cultivating promoter customers, while those with lower tenure months on calls often contribute to customers shifting toward passive and detractor categories.
+
+These two points succinctly capture the key findings related to advisor tenure and its impact on customer sentiment.
+
+Positive Feedback:
+
+Supportive Learning Environment
+
+Mentors and colleagues were highly supportive.
+A conducive atmosphere for professional growth.
+Collegial Collaboration
+
+Strong teamwork and collaboration.
+Colleagues readily shared knowledge and guidance.
+Patient and Encouraging Supervision
+
+Mentors and supervisors were patient and encouraging.
+Positive guidance contributed significantly to learning.
+Areas for Improvement:
+
+Onboarding Clarity
+
+Suggested a centralized resource for complex data elements.
+Useful during the initial weeks to combat information overload.
+Structured Learning Material
+
+Proposed structured resources like handbooks.
+Helpful for newcomers to grasp intricate processes.
+Mentorship Program
+
+Advocated for a formal mentorship program.
+Provides structured learning and knowledge transfer.
+Feedback Mechanism
+
+Recommended a clear feedback mechanism.
+Vital for addressing concerns and enhancing the onboarding experience.
+Conclusion:
+Bupa's commitment to nurturing talent and creating a positive learning environment is commendable. Addressing these areas of improvement can further enhance the onboarding experience for interns, ensuring a seamless transition into their roles and a deeper understanding of the organization's operations.
