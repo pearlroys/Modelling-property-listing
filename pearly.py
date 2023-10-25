8ac2c2f5286d83fb85bd9be3969f272bed6d834a
Pearl Roys <pearl_chisom@yahoo.com>
2023-10-25 12:17:40 +0100

    pearl

--- pearly.py
+++ pearly.py
@@ -1,3 +1,20 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+Here's a clearer summary of the setup:
+
+1. **Backup Sources:**
+   - There are two backup sources:
+     - **Backup PC for the Gateway:** This serves as a backup for the Gateway.
+     - **Backup SIM Card:** This serves as a secondary source of internet connectivity, which can be used if the primary LAN or primary sim connection fails. The router supports two SIM cards by secondary I mean secondary sim.
+
+2. **Levrton's Configuration:**
+   - Leverton has a single control and a single enhanced telemetry system.
+   - Leverton's router like others supports two SIM cards, and both are in use.
+   - One of the SIM cards serves as a backup source for both control and enhanced telemetry. In case of primary connectivity failure, it switches to the backup SIM.
+
+3. **Other Sites' Configuration:**
+   - Other sites also have backup SIM cards, but the backup SIM serves the control telemetry only, not the enhanced telemetry.
+
+4. **Winchester's Configuration:**
+   - Winchester has a backup PC for both control and enhanced telemetry.
+   - Additionally, there is a 4G backup SIM for control telemetry.
+
+In summary, Leverton's configuration is unique in that it uses a single backup SIM for both control and enhanced telemetry. The other sites have backup SIMs that serve as backups for control telemetry only. Winchester has a backup PC for both control and enhanced telemetry an onsite LAN and a separate 4G backup SIM for control telemetry only.
