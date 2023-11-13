7dbdda204babb272963443c0761a9ca6a5b16f45
Pearl Roys <pearl_chisom@yahoo.com>
2023-11-13 09:09:06 +0000

    pearl

--- pearly.py
+++ pearly.py
@@ -1,10 +1,12 @@
-for dictionary in data:
-        keys_with_description = {
-            'description': dictionary.get('description'),
-            'sochigh': dictionary.get('sochigh'),
-            'soclow': dictionary.get('soclow')
-        }
+if "contracts" in data and isinstance(data["contracts"], list):
+        for dictionary in data["contracts"]:
+            if isinstance(dictionary, dict):
+                keys_with_description = {
+                    'description': dictionary.get('description'),
+                    'sochigh': dictionary.get('sochigh'),
+                    'soclow': dictionary.get('soclow')
+                }
 
-        result.append(keys_with_description)
+                result.append(keys_with_description)
 
     return result
