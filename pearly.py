782005524387421799a652690b74dcba6e9e2506
Pearl Roys <pearl_chisom@yahoo.com>
2023-11-14 09:15:39 +0000

    pearl

--- pearly.py
+++ pearly.py
@@ -1,15 +1,24 @@
- if "contracts" in data and isinstance(data["contracts"], list):
-        for dictionary in data["contracts"]:
-            if isinstance(dictionary, dict):
-                description = dictionary.get('description', '')
+import subprocess
 
-                # Check if 'edf' is in the description
-                if 'edf' in description.lower():
-                    parameters = dictionary.get('parameters', {})
-                    keys_with_description = {
-                        'description': description,
-                        'sochigh': parameters.get('sochigh'),
-                        'soclow': parameters.get('soclow')
-                    }
+# List of values to iterate over
+locations = ['london', 'paris', 'newyork', 'tokyo', 'sydney', ...]  # Add more locations as needed
 
-                    result.append(keys_with_description)
+# Output file
+output_file = 'output.txt'
+
+# Loop over the list of locations
+for location in locations:
+    # Command to run
+    command = f'contract-get --env production abc/{location}'
+
+    # Run the command and capture the output
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+
+    # Append the result to the output file
+    with open(output_file, 'a') as file:
+        file.write(f'Result for {location}:\n')
+        file.write(result.stdout)
+        file.write('\n' + '-' * 50 + '\n')
+
+    # Print the result to the console
+    print(f'Result for {location}:\n{result.stdout}\n' + '-' * 50)
