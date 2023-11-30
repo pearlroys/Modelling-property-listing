89da6d03c199163eb4633aa12f8ed1b20e25a183
Pearl Roys <pearl_chisom@yahoo.com>
2023-11-30 16:00:46 +0000

    pearl

--- pearly.py
+++ pearly.py
@@ -1,6 +1,18 @@
-We conducted an investigation and implemented a solution to align the baseline calculator 
-with the optimiser's assumptions regarding state of charge (SOC) after a full outage. 
-This corrective measure aims to prevent the defaulting behavior that previously caused the issue. 
-The fix has been applied by disabling the emergency baselining behavior of the baseline calculator for the optimiser,
- mitigating the risk of conflicts between the two. The solution is now in production, and we 
-will monitor its performance throughout the afternoon to ensure there are no further issues.
+#!/bin/bash
+
+# Run the first script and save the output to a file
+./script1.sh > names1.txt
+
+# Run the second script and save the output to a file
+./script2.sh > names2.txt
+
+# Use comm to find names unique to each list
+unique_names_script1=$(comm -23 names1.txt names2.txt)
+unique_names_script2=$(comm -13 names1.txt names2.txt)
+
+# Print unique names for each script
+echo "Unique Names in Script 1:"
+echo "$unique_names_script1"
+
+echo "Unique Names in Script 2:"
+echo "$unique_names_script2"
